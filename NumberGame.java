f9adc67076cb22a6b929e7733c44bbc333ddc8d0
KASTURI CHATTERJEE <120770926+Kasturi002@users.noreply.github.com>
2024-05-02 21:03:36 +0530

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        int minRange = 1;
+        int maxRange = 100;
+        int attempts = 5;
+        int score = 0;
+
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int remainingAttempts = attempts;
+
+            System.out.println("\nI've selected a number between " + minRange + " and " + maxRange + ".");
+            System.out.println("You have " + remainingAttempts + " attempts to guess it.");
+
+            int guess;
+            boolean guessedCorrectly = false;
+
+            while (remainingAttempts > 0 && !guessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                guess = scanner.nextInt();
+
+                if (guess == randomNumber) {
+                    System.out.println("Congratulations! You guessed the number correctly.");
+                    guessedCorrectly = true;
+                    score += remainingAttempts;
+                } else if (guess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+                remainingAttempts--;
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've run out of attempts. The number was: " + randomNumber);
+            }
+
+            System.out.println("Your score: " + score);
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playChoice = scanner.next();
+
+            playAgain = playChoice.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Thanks for playing!");
+        scanner.close();
+    }
+}
